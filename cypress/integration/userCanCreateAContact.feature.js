93e86906431fe9bfda2e8e67259f5dcf22ef63e1
felixoppongwillborg <felixwillborg@MacBook-Pro-som-tillhor-Felix.local>
2020-06-02 11:25:32 +0200

    added all inputfields and passed tests for it

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -2,5 +2,11 @@ describe ('user can create a contact', () => {
         it ('test', () => {
             cy.visit ('http://localhost:3001')
             cy.get ('#add-contact').click()
+            cy.get ('#name').type('Felix')
+            cy.get ('#email').type('fwlixw@gmail.com')
+            cy.get ('#phone').type('0708574340')
+            cy.get ('#company').type('Craft Academy')
+            cy.get ('#notes').type('Hey hey hey')
+            cy.get ('#twitter').type('@felixoppongwillborg')
         })
 })
